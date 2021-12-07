d4db04d75fa94c9d6557bdb730a1bfdad45e0948
Steve Mbiele <stevembiele@gmail.com>
2021-12-06 23:51:42 -0500

    blocked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { modules } from './config/config.js';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  loadModules() {
+    let dom;
+    console.log(modules);
+    for (let mod in modules) {
+      console.log(modules[mod].module);
+      dom += React.createElement(`${modules[mod].module}`, null, null);
+      // console.log(mod);
+    }
+    // console.log(dom);
+    return dom;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modules: this.loadModules()
+    };
+  }
+
+
+  render() {
+    return (
+      this.state.modules
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+export const modules = [
+  {
+    module: 'Countdown',
+    path: 'src/modules/countdown/',
+  }
+]
